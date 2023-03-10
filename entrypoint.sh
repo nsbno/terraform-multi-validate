e9120997c5ded3f8b0267f5a351a9ad84f1d4e1c
asmundh <aasmund.haugse@gmail.com>
2023-03-10 09:07:34 +0100

    Validate single folder. Feed folders through GA-matrix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 printf "Validating terraform code\n"
-cd ${INPUT_PATH}
-ls -al
-dirs=""
-if [ -z "$INPUT_DIRECTORIES" ]; then
-    dirs=$(find -mindepth ${INPUT_MIN_DEPTH} -maxdepth ${INPUT_MAX_DEPTH} -type d)
-else
-    dirs=$INPUT_DIRECTORIES
+
+if [ -n "$INPUT_PATH" ]; then
+  cd "${INPUT_PATH}" || echo "Invalid or no input path provided."
+  echo "In directory $PWD"
 fi
-for i in ${dirs}; do
+
+if [ -n "$INPUT_FOLDER" ]; then
+  cd "$INPUT_FOLDER" || ( echo "Error, folder $INPUT_FOLDER not found."; exit 1 )
+  echo "In directory $PWD"
+  terraform init -backend=false && terraform validate
+else
+  dirs=""
+
+  if [ -z "$INPUT_DIRECTORIES" ]; then
+    dirs=$(find -mindepth "${INPUT_MIN_DEPTH}" -maxdepth "${INPUT_MAX_DEPTH}" -type d)
+  else dirs=$INPUT_DIRECTORIES; fi
+
+  for i in ${dirs}; do
     (
-    j=$(echo $i | sed 's/\(.*\),/\1 /')
-    echo "env: $j"
-    cd $j
+    j=$(echo "$i" | sed 's/\(.*\),/\1 /') # Remove comma for for-loop
+    echo "Env: $j"
+    cd "$j" || ( echo "Something went wrong when CDing into directory $j"; exit 1 )
+    echo "In directory $PWD"
     terraform init -backend=false && terraform validate
     )
-done
+  done
+fi
